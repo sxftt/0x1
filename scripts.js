d8c1073ad018c5228cd472a8a2b97db07c637646
sxftt <sxfttech@proton.me>
2024-05-03 21:04:05 +0300

    Added experimental things.
    Experiment [1]

--- scripts.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('blur', function() {
-    document.title = '0x1 Device';
-  });
-
-  // Function to restore the original title when the window gains focus
-  window.addEventListener('focus', function() {
-    document.title = '0x1 by sxft';
-  });
